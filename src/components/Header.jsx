17961056d92622c8de148008664c66074622b950
Prachi <prachipramodkumar15@gmail.com>
2024-07-27 20:40:35 +0530

    humburger responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import dog_log from "../assets/dog_log.jpg";
-
+import { RxHamburgerMenu } from "react-icons/rx";
 const Header = () => {
+  const [isShow,setIsShow] = useState(false)
+  const handleToggle =()=>{
+     setIsShow((value)=>!value);
+
+  }
   return (
     <div className="flex justify-between px-[2rem] py-[0.5rem] items-center bg-sky-600">
       <img
@@ -9,13 +14,25 @@ const Header = () => {
         alt="dog logo "
         className="h-[2rem] border-[2px] border-solid border-[#dafc55] rounded-lg p-1  w-[5rem] object-cover"
       />
-      <div className="flex gap-5">
+      
+      <div className="md:flex hidden gap-5 " >
         <p>About Us</p>
         <p>Contact Us</p>
         <p>Found Dog</p>
         <p>Want to Adopt</p>
       </div>
+      <div className="md:hidden flex-cols gap-5 " > 
+          <RxHamburgerMenu onClick={handleToggle}/>
+          
+          <div className={`flex-cols  gap-5 ${isShow===true?"flex-cols":"hidden"} `}>
+            <p>About Us</p>
+            <p>Contact Us</p>
+            <p>Found Dog</p>
+            <p>Want to Adopt</p>
+          </div>
+      </div>
     </div>
+      
   );
 };
 
